7a880df859949508190d0b2a3cefbbe6cd10e40a
ivan.shkurin <ivan.shkurin@matryoshka.com>
2026-01-19 21:51:15 +0600

    chore: remove old tabs directory (migrated to sections)

--- bb_detector/ui/tabs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Tab components for main window."""
-from .play import PlayTab
-from .settings import SettingsTab
-from .calibration import CalibrationTab
-
-__all__ = ['PlayTab', 'SettingsTab', 'CalibrationTab']
