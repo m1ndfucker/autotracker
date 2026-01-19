30491abf9041c811ef537cd65f98006611c577d7
ivan.shkurin <ivan.shkurin@matryoshka.com>
2026-01-19 21:51:02 +0600

    fix(dialogs): enable imports in __init__.py

--- bb_detector/ui/dialogs/__init__.py
+++ bb_detector/ui/dialogs/__init__.py
@@ -1,8 +1,7 @@
 """Modal dialogs."""
 
-# Imports commented out until files are created in later tasks
-# from .profile import ProfileDialog
-# from .milestone import MilestoneDialog
-# from .stats import StatsDialog
+from .profile import ProfileDialog
+from .milestone import MilestoneDialog
+from .stats import StatsDialog
 
 __all__ = ['ProfileDialog', 'MilestoneDialog', 'StatsDialog']
